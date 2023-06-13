13b23628e3155758f1b7ec6014a618168e2d5763
Andres Wagner <andres.wagner@clemerin.com>
2023-06-12 23:11:48 -0400

    Merge remote-tracking branch 'origin/main' into main

--- pages/2_Langchain_Streaming.py
+++ pages/2_Langchain_Streaming.py
@@ -2,11 +2,32 @@ import streamlit as st
 from langchain import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
 
 st.title('🦜🔗 Langchain Streaming App')
 
+template="You are a helpful assistant that translates english to pirate."
+system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+example_human = HumanMessagePromptTemplate.from_template("Hi")
+example_ai = AIMessagePromptTemplate.from_template("Argh me mateys")
+human_template="{text}"
+human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
 with st.sidebar:
   openai_api_key = st.text_input('OpenAI API Key')
+
+chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, example_human, example_ai, human_message_prompt])
+chain = LLMChain(llm=chat, prompt=chat_prompt)
 
 def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
@@ -15,7 +36,7 @@ def generate_response(input_text):
   st.info(resp)
 
 with st.form('my_form'):
-  text = st.text_area('Enter text:', 'What are 3 key advice for learning how to code?')
+  text = st.text_area('Enter text:', 'I love programming.')
   submitted = st.form_submit_button('Submit')
   if submitted:
-    generate_response(text)
+    chain.run(text)
