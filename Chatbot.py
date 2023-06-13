baec7363f1c5e10ef9f36f99e5e3b96a2a00443b
Andres Wagner <andres.wagner@clemerin.com>
2023-06-12 21:40:35 -0400

    Merge pull request #5 from sfc-gh-jcarroll/improve-devcontainer
    Improvements to devcontainer.json

--- Chatbot.py
+++ Chatbot.py
@@ -32,7 +32,7 @@ if user_input and not openai_api_key:
 if user_input and openai_api_key:
     openai.api_key = openai_api_key
     st.session_state.messages.append({"role": "user", "content": user_input})
-    message(user_input, is_user=True)
+    message(user_input, is_user=True, avatar_style="Icons")
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
     msg = response.choices[0].message
     st.session_state.messages.append(msg)
